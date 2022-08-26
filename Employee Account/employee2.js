36fef20f7580dd469922be84d41f1f4076577021
ilampothigai <pothigaipothigai57@gmail.com>
2022-08-26 15:52:54 +0530

    hello

---
+++ Employee Account/employee2.js
@@ -0,0 +1,23 @@
+class Account {
+min_Bal=2000;
+open_Account(){
+    console.log('Account opened Success');
+
+}
+deposite_Amount(){
+    console.log('Account deposit Succesfully');
+
+}
+withdrawal (){
+    console.log('Successfully');
+}
+get_Bal(){
+console.log('Successfully');
+}
+get_st(){
+    console.log('Successfully');
+}
+CloseAccount(){
+    console.log('Successfully');
+}
+}
---
+++ parameter/Employee account/employee1.js
@@ -0,0 +1,7 @@
+class employee {
+eid = 101;
+}
+let e1=new employee();
+let e2=new employee();
+console.log(e1)
+console.log(e2) 
